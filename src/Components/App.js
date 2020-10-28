324d2f048b273bd16fb3a5403de7b87e716b3bf3
mdgarden <chooi9522@gmail.com>
2020-10-29 00:45:32 +0900

    #2.2 React Router part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Router from "./Router";
+import Header from "./Router";
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Header>
         <Router />
-      </>
+      </Header>
     );
   }
 }
