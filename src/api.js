fbaba9bcb570126979a039e62e041b822accc9d1
mdgarden <chooi9522@gmail.com>
2020-11-01 23:40:29 +0900

    #4.3 API Verbs part Two

--- src/api.js
+++ src/api.js
@@ -8,14 +8,33 @@ const api = axios.create({
   },
 });
 
-export const MovieApi = {
+export const MoviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movies", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
 };
 
 export const TvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) => api.get(`tv/${id}`),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
 };
